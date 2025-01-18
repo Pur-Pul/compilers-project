ae772a6c373b1bf6b34426384400785b9e0ffcd6
Pur-Pul <axbergsamuel@gmail.com>
2025-01-18 13:52:20 +0200

    Task 2

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,4 +1,46 @@
 import re
-def tokenize(source_code: str) -> list[str]:
-    model = re.compile(r'([A-Za-z_][A-Za-z0-9_]*|[0-9]+)')
-    return model.findall(source_code)
+from dataclasses import dataclass
+
+@dataclass
+class Source:
+    file: str
+    row: int
+    column: int
+    def __init__(self, file: str, row: int, column: int):
+        self.file = file
+        self.row = row
+        self.column = column
+    def __str__(self) -> str:
+        return f"{self.file}:{self.row}:{self.column}"
+L = Source('',0,0)
+@dataclass
+class Token:
+    text: str
+    type: str
+    source: Source
+    def __init__(self, text : str, type: str, source: Source):
+        self.text = text
+        self.type = type
+        self.source = source
+    def __str__(self) -> str:
+        return f"{self.source}:{self.text}"
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Token):
+            return NotImplemented
+        return self.text == other.text and self.type == other.type and (self.source == other.source or self.source == L or other.source == L)
+
+def tokenize(source_code: str) -> list[Token]:
+    pattern = re.compile(r'([A-Za-z_][A-Za-z0-9_]*|[0-9]+)')
+    rows = source_code.split('\n')
+    tokens: list[Token] = []
+    for row_i, row in enumerate(rows):
+        for match in pattern.finditer(row):
+            col_i = match.start(0)
+            type = "identifier"
+            if match.group(0).isnumeric():
+                type = "int_literal"
+            tokens.append(Token(match.group(0), type, Source('', row_i, col_i)))
+
+    return tokens
+
+#tokenize("if  3\nwhile")
--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -1,5 +1,25 @@
-from compiler.tokenizer import tokenize
+from compiler.tokenizer import tokenize, Token, L
 
 def test_tokenizer_basics() -> None:
-    assert tokenize("if  3\nwhile") == ['if', '3', 'while']
-    assert tokenize("else\nfor token in token_finder\nprint token") == ['else', 'for', 'token', 'in', 'token_finder', 'print', 'token']
+    assert tokenize("if  3\nwhile") == [
+        Token(text='if', type="identifier", source=L), 
+        Token(text='3', type="int_literal", source=L),
+        Token(text='while', type="identifier", source=L),
+    ]
+    assert tokenize("if Token getToken\nelse\nfor token in token_finder\nprint token") == [
+        Token(text='if', type="identifier", source=L),
+        Token(text='Token', type="identifier", source=L),
+        Token(text='getToken', type="identifier", source=L),
+        Token(text='else', type="identifier", source=L),
+        Token(text='for', type="identifier", source=L),
+        Token(text='token', type="identifier", source=L),
+        Token(text='in', type="identifier", source=L),
+        Token(text='token_finder', type="identifier", source=L),
+        Token(text='print', type="identifier", source=L),
+        Token(text='token', type="identifier", source=L),
+        ]
+    assert tokenize('aaa 123 bbb') == [
+        Token(text="aaa", type="identifier", source=L),
+        Token(text="123", type="int_literal", source=L),
+        Token(text="bbb", type="identifier", source=L),
+    ]
