eaec388ef73d393486923a54c1166b46e8f0434e
Pur-Pul <axbergsamue@gmail.com>
2025-01-21 19:28:11 +0200

    Task 3.1

--- src/compiler/parser.py
+++ src/compiler/parser.py
@@ -1,38 +1,45 @@
-from compiler.tokenizer import Token
+from compiler.tokenizer import Token, Source
 import compiler.ast as ast
 
 def parse(tokens: list[Token]) -> ast.Expression:
     pos = 0
 
     def peek() -> Token:
-        if pos < len(tokens):
+        if len(tokens) == 0:
+            return Token(
+                source=Source("",0,0),
+                type="end",
+                text="",
+            )
+        elif pos < len(tokens):
             return tokens[pos]
         else:
             return Token(
-                location=tokens[-1].location,
+                source=tokens[-1].source,
                 type="end",
                 text="",
             )
 
     def consume(expected: str | list[str] | None = None) -> Token:
+        nonlocal pos
         token = peek()
         if isinstance(expected, str) and token.text != expected:
-            raise Exception(f'{token.location}: expected "{expected}"')
+            raise Exception(f'{token.source}: expected "{expected}"')
         if isinstance(expected, list) and token.text not in expected:
             comma_separated = ", ".join([f'"{e}"' for e in expected])
-            raise Exception(f'{token.location}: expected one of: {comma_separated}')
+            raise Exception(f'{token.source}: expected one of: {comma_separated}')
         pos += 1
         return token
     
     def parse_int_literal() -> ast.Literal:
         if peek().type != 'int_literal':
-            raise Exception(f'{peek().location}: expected an integer literal')
+            raise Exception(f'{peek().source}: expected an integer literal')
         token = consume()
         return ast.Literal(int(token.text))
     
     def parse_identifier() -> ast.Identifier:
         if peek().type != 'identifier':
-            raise Exception(f'{peek().location}: expected an identifier')
+            raise Exception(f'{peek().source}: expected an identifier')
         token = consume()
         return ast.Identifier(token.text)
     
@@ -41,6 +48,17 @@ def parse(tokens: list[Token]) -> ast.Expression:
         expr = parse_expression()
         consume(')')
         return expr
+
+    def parse_factor() -> ast.Expression:
+        if peek().text == '(':
+            return parse_parenthesized()
+        match peek().type:
+            case 'int_literal':
+                return parse_int_literal()
+            case 'identifier':
+                return parse_identifier()
+            case _:
+                raise Exception(f'{peek().source}: expected "(", an integer literal or an identifier')
 
     def parse_term() -> ast.Expression:
         left = parse_factor()
@@ -53,17 +71,6 @@ def parse(tokens: list[Token]) -> ast.Expression:
                 right
             )
         return left
-
-    def parse_factor() -> ast.Expression:
-        if peek().text == '(':
-            return parse_parenthesized()
-        match peek().type:
-            case 'int_literal':
-                return parse_int_literal()
-            case 'identifier':
-                return parse_identifier()
-            case _:
-                raise Exception(f'{peek().location}: expected "(", an integer literal or an identifier')
 
     def parse_expression() -> ast.Expression:
         left = parse_term()
@@ -79,7 +86,7 @@ def parse(tokens: list[Token]) -> ast.Expression:
 
     def parse_expression_right() -> ast.Expression:
         left = parse_term()
-        if peek().text in ['+', '-']:
+        if peek().text in ['=']:
             operator_token = consume()
             right = parse_expression_right()
             return ast.BinaryOp(
@@ -90,5 +97,7 @@ def parse(tokens: list[Token]) -> ast.Expression:
         else:
             return left
     
-    return parse_expression()
-
+    main_expression = parse_expression()
+    if peek().type != "end":
+        raise(Exception(f'{peek().source}: garbage at end of expression.'))
+    return main_expression
---
+++ tests/parser_test.py
@@ -0,0 +1,59 @@
+import pytest
+from compiler.parser import parse
+from compiler.tokenizer import Token, Source, L
+import compiler.ast as ast
+
+def test_parse_one_identifier() -> None:
+    tokens = [Token("first", "identifier", L)]
+    assert(parse(tokens)) == ast.Identifier("first")
+
+def test_parse_multiple_identifiers_fails() -> None:
+    tokens = [Token("first", "identifier", Source("filename",5,5)),Token("second", "identifier", Source("filename",5,11))]
+    with pytest.raises(Exception) as e:
+        parse(tokens)
+    assert(e.value.args[0]) == 'filename:5:11: garbage at end of expression.'
+
+def test_parse_one_int_literal() -> None:
+    tokens = [Token("1", "int_literal", L)]
+    assert(parse(tokens)) == ast.Literal(1)
+
+def test_parse_multiple_literals_fails() -> None:
+    tokens = [Token("1", "int_literal", Source("filename",5,5)),Token("2", "int_literal", Source("filename",5,7))]
+    with pytest.raises(Exception) as e:
+        parse(tokens)
+    assert(e.value.args[0]) == 'filename:5:7: garbage at end of expression.'
+
+def test_parse_addition_operation() -> None:
+    tokens = [Token("1", "int_literal", L),Token("+", "operation", L),Token("2", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.Literal(1),"+",ast.Literal(2))
+
+def test_parse_subtraction_operation() -> None:
+    tokens = [Token("1", "int_literal", L),Token("-", "operation", L),Token("2", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.Literal(1),"-",ast.Literal(2))
+
+def test_parse_multiplication_operation() -> None:
+    tokens = [Token("1", "int_literal", L),Token("*", "operation", L),Token("2", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.Literal(1),"*",ast.Literal(2))
+
+def test_parse_division_operation() -> None:
+    tokens = [Token("1", "int_literal", L),Token("/", "operation", L),Token("2", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.Literal(1),"/",ast.Literal(2))
+
+def test_nested_operation_is_left_associative() -> None:
+    tokens = [Token("1", "int_literal", L),Token("+", "operation", L),Token("2", "int_literal", L),Token("-", "operation", L),Token("3", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.BinaryOp(ast.Literal(1),"+",ast.Literal(2)),"-",ast.Literal(3))
+
+def test_in_nested_operation_multiplication_and_division_has_presedence_over_addition_and_subtraction() -> None:
+    tokens = [Token("1", "int_literal", L),Token("+", "operation", L),Token("2", "int_literal", L),Token("*", "operation", L),Token("3", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.Literal(1),"+",ast.BinaryOp(ast.Literal(2),"*",ast.Literal(3)),)
+    tokens = [Token("1", "int_literal", L),Token("+", "operation", L),Token("2", "int_literal", L),Token("/", "operation", L),Token("3", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.Literal(1),"+",ast.BinaryOp(ast.Literal(2),"/",ast.Literal(3)),)
+    tokens = [Token("1", "int_literal", L),Token("-", "operation", L),Token("2", "int_literal", L),Token("*", "operation", L),Token("3", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.Literal(1),"-",ast.BinaryOp(ast.Literal(2),"*",ast.Literal(3)),)
+    tokens = [Token("1", "int_literal", L),Token("-", "operation", L),Token("2", "int_literal", L),Token("/", "operation", L),Token("3", "int_literal", L)]
+    assert(parse(tokens)) == ast.BinaryOp(ast.Literal(1),"-",ast.BinaryOp(ast.Literal(2),"/",ast.Literal(3)),)
+
+def test_empty_tokens_fails_gracefully() -> None:
+    with pytest.raises(Exception) as e:
+        parse([])
+    assert(e.value.args[0]) == ':0:0: expected "(", an integer literal or an identifier'
